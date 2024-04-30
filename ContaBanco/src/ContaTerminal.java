31c2a478b094feaac5361edd5c565d4a94033ea7
Reginaldo <porffiriocosta@gmail.com>
2024-04-30 15:05:55 -0300

    Projeto criado

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.text.MessageFormat;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+        System.out.print("Digite o nome: ");
+        String nome = sc.nextLine();
+        
+		 System.out.print("Por favor, digite o número da Agência: ");
+         String agencia = sc.nextLine();
+         
+		 System.out.print("Digite o numero da conta: ");
+         int conta = Integer.parseInt(sc.nextLine());
+         
+         System.out.print("Digite o nome: ");
+         Double saldo = Double.parseDouble(sc.nextLine());
+         
+         String msg = MessageFormat.format("Olá {0}, obrigado por criar uma conta em nosso banco, sua agência é {1}, conta {2} e seu saldo {3} já está disponível para saque", nome, agencia, conta, saldo);
+         
+         System.out.println(msg);
+	}
+}
